e2897e1588f8d75d7a28d640077d63d50eba4e09
romain2518 <rolenoir25@gmail.com>
2023-02-05 22:47:37 +0100

    Add main slider

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 import dataLoader from "./components/dataLoader.js";
+import mainSlider from "./components/mainSlider.js";
 
 const app = {
     init: function () {
         dataLoader.init();
         
+        mainSlider.init()
+
         console.log('App OK')
     }
 }
