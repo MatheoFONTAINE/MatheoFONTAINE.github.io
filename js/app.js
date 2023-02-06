43290c6c1e518534b7ec6b5ce0fcb4d6088de84e
romain2518 <rolenoir25@gmail.com>
2023-02-06 15:18:14 +0100

    Add project list

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 import dataLoader from "./components/dataLoader.js";
+import filterSystem from "./components/filterSystem.js";
 import mainSlider from "./components/mainSlider.js";
+import projectList from "./components/projectList.js";
+import scrollSnapFixer from "./components/scrollSnapFixer.js";
 
 const app = {
     init: function () {
         dataLoader.init();
+
+        scrollSnapFixer.init();
         
-        mainSlider.init()
+        mainSlider.init();
+        filterSystem.init();
+        projectList.init();
 
         console.log('App OK')
     }
