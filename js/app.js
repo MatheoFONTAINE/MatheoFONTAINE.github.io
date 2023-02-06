859456c0a1d8089fdf9002ab09b6444487c50981
romain2518 <rolenoir25@gmail.com>
2023-02-06 22:53:29 +0100

    Removed scroll snap

--- js/app.js
+++ js/app.js
@@ -4,13 +4,10 @@ import dataLoader from "./components/dataLoader.js";
 import filterSystem from "./components/filterSystem.js";
 import mainSlider from "./components/mainSlider.js";
 import projectList from "./components/projectList.js";
-import scrollSnapFixer from "./components/scrollSnapFixer.js";
 
 const app = {
     init: function () {
         dataLoader.init();
-
-        scrollSnapFixer.init();
         
         mainSlider.init();
         filterSystem.init();
